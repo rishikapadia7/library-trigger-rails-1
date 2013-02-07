393d2d2b59690a959c2d2a8dcb9597ee1236a9bc
Aldrin <aldrin.almacin@mosaic.com>
2013-02-07 14:17:08 -0500

    Renamed and merged steps into their files

--- features/step_definitions/all_page_step.rb
+++
@@ -1,5 +0,0 @@
-Then /^I should see these links:$/ do |table|
-  table.hashes.each do |link_hash|
-    page.should have_selector('a', :text => link_hash[:link])
-  end
-end
--- features/step_definitions/application_step.rb
+++ features/step_definitions/application_step.rb
@@ -54,3 +54,9 @@ end
 Then /^I should see the '(.*)' field$/ do |field_name|
   page.should have_field field_name
 end
+
+Then /^I should see these links:$/ do |table|
+  table.hashes.each do |link_hash|
+    page.should have_selector('a', :text => link_hash[:link])
+  end
+end
