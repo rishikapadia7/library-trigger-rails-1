0f65880cde4b8d186f80c45d1a6ec65d5ebaadc5
Aldrin <aldrin.almacin@mosaic.com>
2013-02-14 13:29:14 -0500

    Both patron and books get checked on the client side

--- app/assets/javascripts/checkout.js
+++ app/assets/javascripts/checkout.js
@@ -1,10 +1,21 @@
 $(document).ready(
   function()
   {
-    $(".patron_search_results form input:radio").change( function()
-      {
-        $(".patron_search_results input:submit").removeAttr('disabled');
-      }
-    );
+    enableSubmitAfterSelect(".patron_search_results"," input:radio");
+    enableSubmitAfterSelect(".book_search_results","input:checkbox");
   }
 );
+
+function enableSubmitAfterSelect(form_class_name, input_type)
+{
+  var selector = (form_class_name + ' ' + input_type);
+  $(selector).change(
+    function()
+    {
+      if($(form_class_name).find("input:submit").attr('disabled'))
+      {
+        $(form_class_name).find("input:submit").attr('disabled',false);
+      }
+    }
+  );
+}
