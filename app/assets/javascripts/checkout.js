16ef37fa63ad585d52ca01cb8e020de82f67c9c1
Aldrin <aldrin.almacin@mosaic.com>
2013-02-25 12:06:39 -0500

    Different book searches is now available

--- app/assets/javascripts/checkout.js
+++ app/assets/javascripts/checkout.js
@@ -2,6 +2,9 @@ $(document).ready(
   function()
   {
     var search_patron_box = $("#search_patron");
+    var books_search_div = $('.book_search_results');
+    var books_selected = $(".selected_books");
+    var patron_search_div = $('.patron_search_results');
 
     search_patron_box.data('oldVal', search_patron_box.val());
 
@@ -11,8 +14,15 @@ $(document).ready(
       }
     });
 
+    patron_search_div.on("change", "input:radio", function(event)
+      {
+        patron_search_div.find('input:submit').attr("disabled",false);
+      }
+    );
+
 
     var search_books_box = $("#search_books");
+    var books_array = new Array;
 
     search_books_box.data('oldVal', search_books_box.val());
 
@@ -21,5 +31,63 @@ $(document).ready(
         $('.book_search_box form').submit();
       }
     });
+
+
+    books_search_div.on("change", "input:checkbox", function(event)
+      {
+        var books = books_search_div.find("input:checkbox");
+        var current_book = $(this);
+        var exists = false;
+
+        if($(this).prop("checked"))
+        {
+          books_array.push($(this).attr("id").substring(6));
+          $(".selected_books p").each(function(i, element)
+            {
+              if(current_book.attr("id") == $($(element).children().get(0)).attr("id"))
+              {
+                exists = true;
+              }
+            }
+          );
+          if(!exists)
+          {
+            $(".selected_books").find('form').prepend(current_book.parent());
+          }
+        }
+      }
+    );
+
+    function checkCheckboxes()
+    {
+      var books = books_selected.find("input:checkbox");
+      if(books.filter(':checked').length > 0)
+      {
+        books_selected.find('input:submit').attr("disabled",false);
+      }
+      else
+      {
+        books_selected.find('input:submit').attr("disabled",true);
+      }
+    }
+
+    setInterval(checkCheckboxes, 33);
+    setInterval(removeDuplicate, 33);
+
+    function removeDuplicate()
+    {
+      books_search_div.find("p").each(function(i,element)
+        {
+          books_selected.find("p").each(function(index, node)
+            {
+              if($(node).find("input:checkbox").prop("id") == $(element).find("input:checkbox").prop("id"))
+              {
+                $(element).remove();
+              }
+            }
+          );
+        }
+      );
+    }
   }
 );
