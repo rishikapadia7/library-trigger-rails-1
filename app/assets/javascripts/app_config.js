6debafd49e9698cbbe0f423e796f7dc4968f635f
Rishi Kapadia <rishi.kapadia@mosaic.com>
2013-04-16 16:36:28 -0400

    Attempted to get offline browsing with native functionality.

---
+++ app/assets/javascripts/app_config.js
@@ -0,0 +1,85 @@
+window.forge = {}; window.forge.config = {
+    "author": "rishi.kapadia@mosaic.com", 
+    "config_hash": "0d3585800bed27cbbab20b78a7640ec7279d89c4", 
+    "config_version": "4", 
+    "core": {
+        "android": {
+            "package_name": "io.trigger.forge9a3fa948924011e2843012313d00dc45"
+        }, 
+        "firefox": {
+            "package_name": "9a3fa948924011e2843012313d00dc45"
+        }, 
+        "general": {
+            "reload": true, 
+            "trusted_urls": [
+                "http://rkap7-mosaic.fwd.wf/*"
+            ]
+        }, 
+        "ie": {
+            "package_name": "{77048922-3389-A2BD-30DB-02CF7E26AC92}"
+        }, 
+        "ios": {
+            "package_name": "com.mosaic.library-app"
+        }, 
+        "osx": {
+            "package_name": "io.trigger.forge9a3fa948924011e2843012313d00dc45"
+        }, 
+        "safari": {
+            "package_name": "forge.safari.library9a3fa948924011e2843012313d00dc45"
+        }
+    }, 
+    "description": "An empty app created by default", 
+    "json_safe_name": "library", 
+    "logging": {
+        "level": "DEBUG"
+    }, 
+    "name": "library", 
+    "package_name": "library9a3fa948924011e2843012313d00dc45", 
+    "platform_version": "v1.4.40", 
+    "plugins": {
+        "contact": {
+            "hash": "notahash"
+        }, 
+        "file": {
+            "hash": "notahash"
+        }, 
+        "media": {
+            "config": {}, 
+            "hash": "notahash"
+        }, 
+        "parse": {
+            "config": {
+                "applicationId": "GPRsaPH25QbByc9V4e0vlFrfVS9tB2t6l3TpBSZj", 
+                "clientKey": "4EQ8dMob1HnJb9bOQ5N8AxJdiAjCsUPPKQwub1Gn"
+            }, 
+            "hash": "notahash"
+        }, 
+        "prefs": {
+            "hash": "notahash"
+        }, 
+        "request": {
+            "config": {
+                "permissions": [
+                    "https://rkap7-mosaic.fwd.wf/*"
+                ]
+            }, 
+            "hash": "notahash"
+        }, 
+        "tabbar": {
+            "hash": "notahash"
+        }, 
+        "tabs": {
+            "hash": "notahash"
+        }, 
+        "topbar": {
+            "hash": "notahash"
+        }, 
+        "ui": {
+            "hash": "notahash"
+        }
+    }, 
+    "trigger_domain": "https://trigger.io", 
+    "uuid": "9a3fa948924011e2843012313d00dc45", 
+    "version": "0.1", 
+    "xml_safe_name": "library"
+};
